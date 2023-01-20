2a00fbeabc2e71581536b7efcba201f176c900ab
SangGyu-Lim <87923181+SangGyu-Lim@users.noreply.github.com>
2023-01-20 15:40:37 +0900

    [level 1] Title: 예산, Time: 1.70 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12982. 예산/예산.cs
@@ -0,0 +1,23 @@
+using System;
+
+public class Solution {
+    public int solution(int[] d, int budget) {
+        int answer = 0;
+        
+        Array.Sort(d);
+        for(int i = 0; i < d.Length; ++i)
+        {
+            if(budget >= d[i])
+            {
+                budget -= d[i];
+                ++answer;
+            }
+            else
+            {
+                break;
+            }
+        }
+        
+        return answer;
+    }
+}
